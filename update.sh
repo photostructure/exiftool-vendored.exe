aae6ed0be5f60f19aeccad6d054a0b2277fe105c
Matthew McEachen <matthew@photostructure.com>
2022-06-25 13:50:21 -0700

    shfmt and delint update.sh. no-op if no pending changes.

--- update.sh
+++ update.sh
@@ -3,11 +3,20 @@
 # Updates to the latest version of exiftool. Assumes we can git clone exiftool
 # into the parent directory.
 
-if [ ! -d ../exiftool ] ; then
-  (cd .. ; git clone git@github.com:exiftool/exiftool.git)
+if [ ! -d ../exiftool ]; then
+  (
+    cd ..
+    git clone git@github.com:exiftool/exiftool.git
+  )
 fi
 
-(cd ../exiftool ; git add . ; git stash ; git checkout master ; git pull)
+(
+  cd ../exiftool
+  git add .
+  git stash -u
+  git checkout master
+  git pull
+)
 
 rm -rf bin
 mkdir -p bin/exiftool_files
@@ -15,11 +24,11 @@ mkdir -p bin/exiftool_files
 # Pull down Oliver's supplemental files:
 mkdir -p .dl
 DEST=.dl/pfe.zip
-if [ ! -f $DEST ] ; then
+if [ ! -f $DEST ]; then
   curl -o $DEST https://oliverbetz.de/cms/files/Artikel/ExifTool-for-Windows/perl-5.30.0.1_for_ExifTool.zip
 fi
 
-if [ "$(sha1sum $DEST | cut -d' ' -f1 )" != "1cf1c2293e703bb0cd7673ae051ed08d1eb64952" ] ; then
+if [ "$(sha1sum $DEST | cut -d' ' -f1)" != "1cf1c2293e703bb0cd7673ae051ed08d1eb64952" ]; then
   echo "checksum did not match for $DEST. Exitting."
   rm $DEST
   exit 1
@@ -30,13 +39,19 @@ cp -r ppl/pplx.exe bin/exiftool.exe
 unzip -q $DEST -d bin
 
 cp -rp ../exiftool/* bin/exiftool_files
-(cd bin/exiftool_files ; rm -rf t html MANIFEST META* perl-Image-ExifTool.spec validate build_tag_lookup pp_build_exe.args)
+(
+  cd bin/exiftool_files
+  rm -rf t html MANIFEST META* perl-Image-ExifTool.spec validate build_tag_lookup pp_build_exe.args
+)
 
 # windows-specific start script
 rm bin/exiftool_files/exiftool
 mv bin/exiftool_files/windows_exiftool bin/exiftool_files/exiftool.pl
 
-# exiftool never has a patch version:
-NEWVER=$(../exiftool/exiftool -ver).0-pre
-
-yarn version --new-version "$NEWVER"
+if [ "$(git status --porcelain=v1 2>/dev/null | wc -l)" -eq 0 ]; then
+  echo "No-op: already up to date"
+else
+  # exiftool never has a patch version:
+  NEWVER=$(../exiftool/exiftool -ver).0-pre
+  yarn version --new-version "$NEWVER"
+fi
